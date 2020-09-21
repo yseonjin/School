17be688063206ef8752ec7867edbac0835cc6589
Jinie <71276587+yseonjin@users.noreply.github.com>
2020-09-21 17:58:17 +0900

    Add files via upload

--- list1005.php
+++ list1005.php
@@ -1,9 +1,9 @@
 <?include('top.php'); ?>
 <section>
-<br>
-<div align=center>
-<meta charset="UTF-8">
-<?
+    <br>
+    <div align="center">
+        <meta charset="UTF-8">
+    <?
     if ( empty( $_GET['idx'] ) ) {
 	    $idx = 0;
 	} else {
@@ -16,7 +16,7 @@ $page_size = 10 ;   // 페이지 당 개수
 // 2) 페이지 List 사이즈 
 $page_list_size = 10;  // 페이지 List 사이즈 
 
-$con = mysqli_connect("localhost","root","autoset","majustory");
+$con = mysqli_connect("localhost","root","world63","majustory");
 $query = "select * from school1004  limit  $idx , $page_size " ;
 $result = mysqli_query($con,$query);
 
@@ -26,7 +26,7 @@ $result_count = mysqli_query($con,$total_count);
 $result_data = mysqli_fetch_array($result_count ,MYSQLI_ASSOC );
 $total_row = $result_data['tc'];
 ?>
-<? 
+        <? 
  // 4) 총 페이지 수
  $total_page = ceil($total_row/$page_size) ;
 
@@ -44,33 +44,82 @@ $end_page = $start_page + $page_list_size -1 ;
 
 ?>
 
-1.페이지사이즈 :<?=$page_size?>  &emsp;
-2.페이지 List Size: <?=$page_list_size?>  &emsp;
-3.총 레코드수: <?=$total_row?>  &emsp;
-4.총 페이지 수: <?=$total_page?>  &emsp; <br>
-5.현재레코드: <?=$idx?> &emsp;  
-6.현재페이지:  <?=$current_page?> &emsp;
-7.하단 가로 시작페이지: <?=$start_page?> &emsp;
-8.하단 가로 마지막 페이지:  <?=$end_page?> &emsp;
+        1.페이지사이즈 :<?=$page_size?>
+        &emsp; 2.페이지 List Size:
+        <?=$page_list_size?>
+        &emsp; 3.총 레코드수:
+        <?=$total_row?>
+        &emsp; 4.총 페이지 수:
+        <?=$total_page?>
+        &emsp;
+        <br>
+        5.현재레코드:
+        <?=$idx?>
+        &emsp; 6.현재페이지:
+        <?=$current_page?>
+        &emsp; 7.하단 가로 시작페이지:
+        <?=$start_page?>
+        &emsp; 8.하단 가로 마지막 페이지:
+        <?=$end_page?>
+        &emsp;
 
-
-<table width=500 border=1>
-<tr><td>번호 </td><td>이름 </td><td> 제목 </td> </tr>
-<?
+        <table width="500" border="1">
+            <tr>
+                <td>번호
+                </td>
+                <td>이름
+                </td>
+                <td>
+                    제목
+                </td>
+            </tr>
+            <?
 while( $data = mysqli_fetch_array($result ,MYSQLI_ASSOC )) {
 ?>
-<tr  bgcolor=#FFCA980>
-<td > <?=$data['sno']?> </td>
-<td> <?=$data['sname']?></td>
-<td> <?=$data['title']?> </td>  
-</tr>
-
-
-<?
+            <tr bgcolor="#FFCA980">
+                <td >
+                    <?=$data['sno']?>
+                </td>
+                <td>
+                    <?=$data['sname']?></td>
+                <td>
+                    <?=$data['title']?>
+                </td>
+            </tr>
+            <?
    }
    mysqli_close ($con);
 ?>
-</table>
-</section>
-<?include('bottom.php');?>
-​
+        </table>
+
+        <? 
+//  (11 - 2) * 10 ,  ( 21 - 2) * 10 , (31 -2) * 10
+//      idx=90    ,   idx=190       ,  idx=290
+if($start_page>$page_list_size){
+   $prev = ($start_page-2)*$page_size;
+   echo "<a href='list1005.php?idx=$prev'>[이전10페이지]</a>&nbsp;";
+}
+?>
+
+        <?
+// [1] [2] [3] , ....., [9] [10]
+//  1, 11, 21 , .... , 81, 91
+for($i=$start_page;$i<=$end_page;$i++){
+
+   if($i <= $total_page) {   // [i] 값은 총 페이지수 보다 클 수 없다.!! 
+      $page = ($i-1)*$page_size+1;  
+      echo "<a href='list1005.php?idx=$page'>[$i]</a>&nbsp;";
+   }
+}
+?>
+
+        <?
+// idx =1 , idx=101 , idx=201 ... 
+if($end_page<$total_page){
+  $next = $end_page*$page_size+1;
+  echo "<a href='list1005.php?idx=$next'>[다음10페이지]</a>&nbsp;";
+}
+?>
+
+    </section>
+    <?include('bottom.php');?>
